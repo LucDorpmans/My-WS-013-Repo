1d00dc8a09560a16fd5fafdcb4ca504220ca26f0
LucDorpmans <GitHub@sure.nl>
2021-05-24 16:16:33 +0200

    Add files via upload

--- GetMy-WS013-Scripts.ps1
+++ GetMy-WS013-Scripts.ps1
@@ -3,9 +3,9 @@ Function Get-MyScript
 {    [CmdletBinding()]
     Param    ( 
         [Parameter(Mandatory=$true,Position=0)]
-        [string]$AFile,
+        [string]$AScript,
         [string]$SaveLocation = "$env:USERPROFILE\Downloads\"     )
-        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AFile"  -Outfile "$SaveLocation$AFile" }
+        Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SaveLocation$AScript" }
 
 Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "WAC-Download+Install.ps1"
