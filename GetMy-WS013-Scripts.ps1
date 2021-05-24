7feb8c5458b36bf733f2a352787c7d691ab679b6
LucDorpmans <GitHub@sure.nl>
2021-05-25 01:45:57 +0200

    Add files via upload

--- GetMy-WS013-Scripts.ps1
+++ GetMy-WS013-Scripts.ps1
@@ -1,27 +1,9 @@
 # GetMy-WS013-Scripts.ps1
-Function Get-MyScript ()
-{  Param ( [Parameter(Mandatory=$true,Position=0)][string]$AScript,
-           [string]$SaveLocation = "$env:USERPROFILE\Downloads\"    )
-   Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SaveLocation$AScript" 
-   PSEdit  ("$env:USERPROFILE\Downloads\$AScript") }
+Function Get-MyScript { Param( [string]$AScript,
+							   [string]$SPath = "$env:USERPROFILE\Downloads\")
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SPath$AScript" }
 
 Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "WAC-Download+Install.ps1"
-Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
+Get-MyScript "EdgeMSI-DownloadComplete.ps1"
 Get-MyScript "Edge-InstallOnly.ps1"
-
-<#
-# Module 2:
-
-# Module 3:
-
-# Module 4:
-Get-MyScript Mod4A-LabVM-Instructions.ps1
-Get-MyScript Mod4A-LabVM-LabConfig.ps1
-Get-MyScript Mod4A-LabVM-Scenario_Part1.ps1
-
-Get-MyScript Mod4A-Mgmt-MultiNodeConfig.psd1
-Get-MyScript Mod4A-Mgmt-SDNExpress.ps1
-Get-MyScript Mod4A-Mgmt-Scenario_Part2.ps1
-
-#>
