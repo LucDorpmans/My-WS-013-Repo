a82ca4261b770edb70a68c6f918b1ced0dd4fd11
LucDorpmans <GitHub@sure.nl>
2021-05-25 23:53:15 +0200

    Add files via upload

---
+++ Labs/Mod2/Mod2-MyDC-LabInstructions.ps1
@@ -0,0 +1,44 @@
+﻿$servers = @('S2D1','S2D2','S2D3','S2D4')
+$features = 'Hyper-V', 'Failover-Clustering', 'Data-Center-Bridging', 'RSAT-Clustering-PowerShell', 'Hyper-V-PowerShell', 'FS-FileServer'
+Invoke-Command ($servers) {
+ Install-WindowsFeature -Name $using:features
+ }
+
+Invoke-Command ($servers) {
+  Restart-Computer -Force
+  }
+
+Invoke-Command ($servers) {
+ Update-StorageProviderCache
+  Get-StoragePool | ? IsPrimordial -eq $false | Set-StoragePool -IsReadOnly:$false -ErrorAction SilentlyContinue
+  Get-StoragePool | ? IsPrimordial -eq $false | Get-VirtualDisk | Remove-VirtualDisk -Confirm:$false -ErrorAction SilentlyContinue
+  Get-StoragePool | ? IsPrimordial -eq $false | Remove-StoragePool -Confirm:$false -ErrorAction SilentlyContinue
+  Get-PhysicalDisk | Reset-PhysicalDisk -ErrorAction SilentlyContinue
+  Get-Disk | ? Number -ne $null | ? IsBoot -ne $true | ? IsSystem -ne $true | ? PartitionStyle -ne RAW | % {
+      $_ | Set-Disk -isoffline:$false
+      $_ | Set-Disk -isreadonly:$false
+      $_ | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false
+      $_ | Set-Disk -isreadonly:$true
+      $_ | Set-Disk -isoffline:$true
+  }
+  Get-Disk | Where Number -Ne $Null | Where IsBoot -Ne $True | Where IsSystem -Ne $True | Where PartitionStyle -Eq RAW | Group -NoElement -Property FriendlyName
+  } | Sort -Property PsComputerName, Count
+
+
+Test-Cluster -Node 'S2D1','S2D2','S2D3','S2D4' -Include 'Storage Spaces Direct', 'Inventory', 'Network', 'System Configuration'
+
+New-Cluster -Name 'S2DCL1' -Node 'S2D1','S2D2','S2D3','S2D4' -NoStorage
+
+
+# Task 4:
+
+Enable-ClusterStorageSpacesDirect -CimSession 'S2DCL1'
+
+
+# Task 5 (!Run these in Azure PowerShell Cli!):
+Register-AzResourceProvider -ProviderNamespace Microsoft.Insights  
+Register-AzResourceProvider -ProviderNamespace Microsoft.AlertsManagement
+
+Get-AzResourceProvider -ProviderNamespace Microsoft.Insights  
+Get-AzResourceProvider -ProviderNamespace Microsoft.AlertsManagement
+
---
+++ Labs/Mod2/Mod2-MyHostLabInstructions.ps1
@@ -0,0 +1,25 @@
+﻿# Task 1
+Get-ChildItem -Path F:\WSLab-master\ -File -Recurse | Unblock-File
+
+Set-Location -Path F:\WSLab-master\Scripts
+
+Move-Item -Path '.\LabConfig.ps1' -Destination '.\LabConfig.m2l0.ps1' -Force -ErrorAction SilentlyContinue
+Move-Item -Path '.\Scenario.ps1' -Destination '.\Scenario.m2l0.ps1' -Force -ErrorAction SilentlyContinue
+
+Copy-Item -Path 'F:\WSLab-master\Scenarios\S2D and Cloud Services Onboarding\Scenario.ps1' -Destination '.\'
+Copy-Item -Path 'F:\WSLab-master\Scenarios\S2D and Cloud Services Onboarding\Labconfig.ps1' -Destination '.\'
+
+
+Get-VM | Where-Object Name -ne 'WSLabOnboard-DC' | Start-VM
+
+CORP\LabAdmin
+LS1setup!
+
+# Task 2
+$VMs = @('WSLabOnboard-S2D1','WSLabOnboard-S2D2','WSLabOnboard-S2D3','WSLabOnboard-S2D4')
+Stop-VM -VMName $VMs -Force
+
+Set-VMProcessor -VMName $VMs -ExposeVirtualizationExtensions $true
+Set-VM $VMs -ProcessorCount 2 -StaticMemory -MemoryStartupBytes 4GB
+Start-VM -VMName $VMs
+
