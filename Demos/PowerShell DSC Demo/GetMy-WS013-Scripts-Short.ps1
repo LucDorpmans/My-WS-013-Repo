b0e4558b69c83ba6049923f3415da92ab21d7d50
LucDorpmans <GitHub@sure.nl>
2021-05-26 00:25:02 +0200

    Commit from Local

--- Demos/PowerShell DSC Demo/GetMy-WS013-Scripts-Short.ps1
+++
@@ -1,9 +0,0 @@
-# GetMy-WS013-Scripts-Short.ps1
-Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False, 
-							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AFile"  -Outfile "$SPath$AFile" }
-
-Get-MyScript "Chrome-Download+Run-Installer.ps1" -EditFile
-Get-MyScript "WAC-Download+Install.ps1" -EditFile
-Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
-Get-MyScript "Edge-InstallOnly.ps1"
