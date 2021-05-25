f4388fb1b1611ab92b590da2cd060bde4bbd6656
LucDorpmans <GitHub@sure.nl>
2021-05-26 01:41:06 +0200

    Add files via upload

--- Demos/PowerShellDSCDemo/DemoPowerShellDSC.ps1
+++ Demos/PowerShellDSCDemo/DemoPowerShellDSC.ps1
@@ -1,13 +1,19 @@
 ﻿# 1. On the Azure VM, open PowerShell ISE, copy the file IISConfig.ps1 to C:\DemoFiles\
 New-Item -Name DemoFiles\ -Path C:\ -ItemType Directory 
 Copy-Item $env:USERPROFILE\Documents\IISConfig.ps1 -Destination C:\DemoFiles\IISConfig.ps1 -Force
+Copy-Item C:\Library\Demos\IISConfig.ps1 -Destination C:\DemoFiles\IISConfig.ps1 -Force
+
 PSEdit C:\DemoFiles\IISConfig.ps1 
 
+Install-PackageProvider nuget -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 # 2. From the PowerShell ISE console pane, run the following command to install the xWebAdministration module:
 Install-Module xWebAdministration -Verbose 
 #    Note: When you receive a prompt to install NuGet provider, select Yes. When you receive a prompt to install the modules from PSGallery, select Yes to All.
 
-# 3. In the PowerShell ISE, run the script you copied into the script pane.
+# 3. Run the IISConfig script you copied into the script pane.
+Invoke-Expression C:\DemoFiles\IISConfig.ps1 
+
 #	 Note the message displayed in the script pane, and then verify that the C:\DemoFiles\IISConfig\localhost.mof file has been successfully created.
 PSEdit C:\DemoFiles\IISConfig\localhost.mof
 
