c291eda769f6b306590fb70f28e18ff8f726c815
LucDorpmans <GitHub@sure.nl>
2021-05-25 02:00:28 +0200

    Add files via upload

--- GetMy-WS013-Scripts.ps1
+++ GetMy-WS013-Scripts.ps1
@@ -1,9 +1,9 @@
 # GetMy-WS013-Scripts.ps1
-Function Get-MyScript { Param( [string]$AScript,
-							   [string]$SPath = "$env:USERPROFILE\Downloads\")
+Function Get-MyScript () 
+			{ Param( [string]$AScript,[string]$SPath = "$env:USERPROFILE\Downloads\")
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SPath$AScript" }
 
 Get-MyScript "Chrome-Download+Run-Installer.ps1"
 Get-MyScript "WAC-Download+Install.ps1"
-Get-MyScript "EdgeMSI-DownloadComplete.ps1"
+Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
 Get-MyScript "Edge-InstallOnly.ps1"
