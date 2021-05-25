7061cc58d977eba2ae0de11b65e0737ab552a23f
LucDorpmans <GitHub@sure.nl>
2021-05-26 00:13:07 +0200

    Delete GetMy-WS013-Scripts.ps1

--- GetMy-WS013-Scripts.ps1
+++
@@ -1,9 +0,0 @@
-# GetMy-WS013-Scripts.ps1
-Function Get-MyScript { Param( [string]$AScript,[string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SPath$AScript" 
-			PSEdit  ("$SPath$AScript")}
-
-Get-MyScript "Chrome-Download+Run-Installer.ps1"
-Get-MyScript "WAC-Download+Install.ps1"
-Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
-Get-MyScript "Edge-InstallOnly.ps1"
