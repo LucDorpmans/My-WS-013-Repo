2aa4e8d151ee9be717f2b68b338917bb982da0e9
LucDorpmans <GitHub@sure.nl>
2021-05-26 15:56:17 +0200

    Add files via upload

--- GetMy-WS013-Scripts.ps1
+++ GetMy-WS013-Scripts.ps1
@@ -1,8 +1,8 @@
 # GetMy-WS013-Scripts.ps1
 Function Get-MyScript { Param( [string]$AScript,[switch]$EditFile = $False, 
 							   [string]$SPath = "$env:USERPROFILE\Downloads\")
-			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SPath$AScript" }
-			If ($EditFile) { PSEdit ("$SPath$AScript" )} 
+			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-013-Repo/main/$AScript"  -Outfile "$SPath$AScript" 
+If ($EditFile) { PSEdit ("$SPath$AScript" )} }
 
 Get-MyScript "Chrome-Download+Run-Installer.ps1"  -EditFile
 Get-MyScript "WAC-Download+Install.ps1"
